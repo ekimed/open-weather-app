e0162a2dae51a0d8f5697acb4edb038ac777865b
Eunice Kim <ekimed@gmail.com>
2015-08-26 12:37:53 -0700

    fixed bug added animation

--- app/owm-app.js
+++ app/owm-app.js
@@ -9,8 +9,9 @@ angular.module('OWMApp', ['ngRoute'])
 			controller: 'CityCtrl', 
 			resolve: {
 				city: function (owmCities, $route, $location) {
-					var city = $route.current.params.city;
-					if (owmCities.indexOf(city === -1)) {
+					var city = $route.current.params.city.replace('_', ' ');
+					if (owmCities.indexOf(city) === -1) {
+						console.log('error')
 						$location.path('/error');
 					}
 					return city;
@@ -21,14 +22,23 @@ angular.module('OWMApp', ['ngRoute'])
 			template: '<p>Error - Page Not Found</p>'
 		});
 	}])
-	.run(function ($rootScope, $location) {
+	.run(function ($rootScope, $location, $timeout) {
 		$rootScope.$on('$routeChangeError', function () {
 			$location.path('/error');
+		});
+
+		$rootScope.$on('$routeChangeStart', function () {
+			$rootScope.isLoading = true;
+		});
+		$rootScope.$on('$routeChangeSuccess', function () {
+			$timeout(function () {
+				$rootScope.isLoading = false;
+			}, 1000);
 		});
 	})
 	.controller('HomeCtrl', function ($scope) {
 
 	})
-	.controller('CityCtrl', function ($scope, $routeParams) {
+	.controller('CityCtrl', function ($scope, city) {
 		$scope.city = city;
 	});
